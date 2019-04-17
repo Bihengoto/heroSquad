5509306e7e8dab65409663df0c8c64cc8e36c318
Bihengoto <isaacbihengoto@gmail.com>
2019-04-17 11:11:40 +0300

    added the squad form code and its respective template code in the app file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,17 +21,17 @@ public class App {
 
         port(port);
 
-//        get("/", (request, response) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            model.put("template", "templates/index.vtl" );
-//            return new ModelAndView(model, "templates/layout.vtl");
-//        }, new VelocityTemplateEngine());
-
-        get("/squad", (request, response) -> {
+        get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("template", "templates/squad_form.vtl" );
+            model.put("template", "templates/index.vtl" );
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+//        get("/squad", (request, response) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            model.put("template", "templates/squad_form.vtl" );
+//            return new ModelAndView(model, "templates/layout.vtl");
+//        }, new VelocityTemplateEngine());
 
         post("/squad", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
