f9606ac01b6995339c7e3b4425be9c51c7bb132e
Bihengoto <isaacbihengoto@gmail.com>
2019-04-17 11:43:43 +0300

    added the hero code for displaying the content at its respective template code and the code in the app file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -97,20 +97,20 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
-//        get("/heroes/:id", (request, response) -> {
-//            Map<String, Object> model = new HashMap<String, Object>();
-//            Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
-//            model.put("hero", hero);
-//            model.put("template", "templates/hero.vtl");
-//            return new ModelAndView(model, layout);
-//        }, new VelocityTemplateEngine());
-
-        get("/heroes", (request, response) -> {
+        get("/heroes/:id", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("heroes", Hero.all());
-            model.put("template", "templates/heroes.vtl");
+            Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+            model.put("hero", hero);
+            model.put("template", "templates/hero.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+//        get("/heroes", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            model.put("heroes", Hero.all());
+//            model.put("template", "templates/heroes.vtl");
+//            return new ModelAndView(model, layout);
+//        }, new VelocityTemplateEngine());
 
         post("/heroes", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
